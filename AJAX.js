6dff7ed79bf0bc8ebda5d1ae2c6a7d1a26364d98
saniaspence <saniaspence90210@gmail.com>
2018-11-13 22:36:52 -0500

    Exercise 3

--- AJAX.js
+++ AJAX.js
@@ -1,14 +1,15 @@
 "use strict";
- function loadDefinitions()
- {
-     var xhttp = new XMLHttpRequest();
-     xhttp.onreadystatechange = function() 
-     {
-         if (this.readyState == 4 && this.status == 200) 
-         {
-             alert(this.responseText);
-         }
-     };
-     xhttp.open("GET", "request.php?q="+"definition", true);
-     xhttp.send();
- }
+function loadDefinitions()
+{
+    var query = document.getElementById("queryTxtBox").value.toLowerCase();
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() 
+    {
+        if (this.readyState == 4 && this.status == 200) 
+        {
+            document.getElementById("result").innerHTML = this.responseText;
+        }
+    };
+    xhttp.open("GET", "request.php?q="+ query, true);
+    xhttp.send();
+}
