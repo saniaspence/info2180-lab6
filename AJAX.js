e0b7a08e30453cd33ef47cab0478872e48a76718
saniaspence <saniaspence90210@gmail.com>
2018-11-13 23:02:45 -0500

    Exercise 4

--- AJAX.js
+++ AJAX.js
@@ -1,5 +1,5 @@
 "use strict";
-function loadDefinitions()
+function loadDefinitions(allSearch)
 {
     var query = document.getElementById("queryTxtBox").value.toLowerCase();
     var xhttp = new XMLHttpRequest();
@@ -7,9 +7,52 @@ function loadDefinitions()
     {
         if (this.readyState == 4 && this.status == 200) 
         {
-            document.getElementById("result").innerHTML = this.responseText;
+            parseXMLData(this, allSearch, query);
         }
     };
-    xhttp.open("GET", "request.php?q="+ query, true);
+    xhttp.open("GET", "request.php", true);
     xhttp.send();
+}
+
+function parseXMLData(xml, allSearch, query) 
+{
+    var i;
+    var xmlDoc = xml.responseXML;
+    var allDef="<ol>";
+    var x = xmlDoc.getElementsByTagName("definition");
+    var notFound = true;
+    
+    if (allSearch) // Returns all definitions
+    {
+        for (i = 0; i <x.length; i++) 
+        { 
+            allDef += "<li>" + "<h3>" + x[i].getAttributeNode("name").value + "</h3>" + // prints the name attribute of the definition
+            "<p>" + x[i].childNodes[0].nodeValue + "</p>" +                             // prints the definition of the word
+            "<p> - " + x[i].getAttributeNode("author").value + "</p>" +                 // prints the name attribute of the definition
+            "</li>";  
+        }
+        allDef += "</ol>";
+        document.getElementById("result").innerHTML = allDef;
+    }
+    else // Returns only a single entry
+    {
+        for (i = 0; i <x.length; i++) 
+        { 
+            if (x[i].getAttributeNode("name").value.toLowerCase() == query)
+            {
+                document.getElementById("result").innerHTML  = "<ol><li>" + "<h3>" + x[i].getAttributeNode("name").value + "</h3>" + 
+                "<p>" + x[i].childNodes[0].nodeValue + "</p>" +                             
+                "<p> - " + x[i].getAttributeNode("author").value + "</p>" +                 
+                "</li></ol>";
+                notFound = false;
+                break;
+            }
+            
+        }
+        
+        if(notFound) // No results found
+        {
+            document.getElementById("result").innerHTML  = "<h3>No Results found for: '" + query + "'</h3>";
+        }
+    }
 }
